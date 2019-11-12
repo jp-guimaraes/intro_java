eeaf8776873ddef71a2ebf3c9b2734ea7bd94030
João Paulo F. Guimarães <joao.paulo.f.guimaraes@gmail.com>
2019-11-12 18:18:40 -0300

    começando lista 3

---
+++ lista3/Exemplos/exemplo_01.java
@@ -0,0 +1,18 @@
+public class exemplo_01 {
+    public static void main(String[] args) {
+        
+        int contador = -5;
+        
+        
+        while(contador < 8){
+            
+            
+            contador = contador + 1;
+            System.out.println(contador);
+        }       
+
+        
+
+    }
+}
+
