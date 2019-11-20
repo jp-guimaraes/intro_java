ad1b31d1b31deb21f98fc9ceaa734bc7a90cd404
João Paulo F. Guimarães <joao.paulo.f.guimaraes@gmail.com>
2019-11-20 13:22:26 -0300

    começando lista 4

---
+++ lista4/Exemplos/exemplo_01.java
@@ -0,0 +1,11 @@
+public class exemplo_01 {
+    static void funcao() {
+        System.out.println("Estou dentro da minha função!");
+    }
+    
+    
+    public static void main(String[] args) {        
+        funcao();        
+    }
+}
+
