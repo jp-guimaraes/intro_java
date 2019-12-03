7f4402f9ac65fe90c27a8e3178a499c13da441a2
João Paulo F. Guimarães <joao.guimaraes@ifrn.edu.br>
2019-12-03 10:02:02 -0300

    colocnado repsostas

---
+++ lista4/Respostas/q1.java
@@ -0,0 +1,15 @@
+public class q1 {
+    public static double nota(double a, double b){
+        double media = (a+b)/2;
+        return media;
+    }
+    public static void main(String[] args) {
+        double nota1 = 10;
+        double nota2 = 4;
+
+        double x = nota(nota1, nota2);
+
+        System.out.println(x);
+    }
+}
+
